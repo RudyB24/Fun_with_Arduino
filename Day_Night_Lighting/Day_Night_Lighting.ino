16195adfefc9d3f9e6d932bc0554d233843c438c
Rudy Boer <47658556+RudyB24@users.noreply.github.com>
2022-03-11 15:20:55 +0100

    Update Day_Night_Lighting.ino

--- Day_Night_Lighting/Day_Night_Lighting.ino
+++ Day_Night_Lighting/Day_Night_Lighting.ino
@@ -1,8 +1,11 @@
 // Ruud Boer - March 2022
 
-//--------------------------------------------------------------------------------------------------
-// In te vullen waarden:
-
+// Light presets, defined by RGBW values in the table below:
+// 0 Work light
+// 1 Day
+// 2 Evening
+// 3 Night
+// 4 Morning
 //                        RRR,GGG,BBB,WWW       light_state
 byte light_state[5][4] ={{255,255,255,255},  // 0 Work light
                          {255,  0,  0,  0},  // 1 Day
@@ -11,39 +14,23 @@ byte light_state[5][4] ={{255,255,255,255},  // 0 Work light
                          {  0,  0,  0,255}}; // 4 Morning
 
 int fade_interval = 10; // [ms] the higher the number, the slower the fade. Value 10 equals 2,5 sec.
-
 unsigned int light_state_time[] = {10,4,10,4}; // [s] day, evening, night, morning 
 
+byte state_input[]   = {2,3,4,5,7,8}; // LOW is active. [0]-[4] switch to one of the 5 states, [5] switches the sequencer
+byte LED_output[] = {6,9,10,11};   // Note: these must be PWM outputs
+
+//--------------------------------------------------------------------------------------------------
+// No need to change anyting below
 //--------------------------------------------------------------------------------------------------
 
-// Control for Model Railroud Layout LED lightins
-// User defined states, defined by RGBW values:
-// 0 Work light
-// 1 Day
-// 2 Evening
-// 3 Night
-// 4 Morning
-
-// Inputs (switch to GND) are used to:
-// - Switch to one of the 5 states
-// - Start sequencer
-
 byte i, sequencer, state, test, R,G,B,W, Rtarget, Gtarget, Btarget, Wtarget;
-
-//                   0,1,2,3,4,5   // light_state number
-byte TC_input[]   = {2,3,4,5,7,8}; // TrainController inputs (pushbuttons)
-
-byte LED_output[] = {6,9,10,11};
-
-
-
 unsigned long time_to_change_state, time_for_fade_step;
 
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, 0);
   for (i = 0; i < 6; i++) {
-    pinMode(TC_input[i], INPUT_PULLUP);
+    pinMode(state_input[i], INPUT_PULLUP);
   }
   Serial.begin(9600);
   Serial.println("Started");
@@ -60,7 +47,7 @@ void setRGBW(byte s) {
 void loop() {
 
   for (i = 0; i < 6; i++) {
-    if(!digitalRead(TC_input[i])) {
+    if(!digitalRead(state_input[i])) {
       if(i==5) {
         sequencer = 1;
         state = 4;
