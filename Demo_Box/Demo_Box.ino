5948241934dfdb1e0997dfe971e535ff61bea5c5
Rudy Boer <47658556+RudyB24@users.noreply.github.com>
2023-07-06 13:35:23 +0200

    Update Demo_Box.ino

--- Demo_Box/Demo_Box.ino
+++ Demo_Box/Demo_Box.ino
@@ -1,222 +1,274 @@
-// Ruud Boer, March 2022
-// Demo box with LEDs, pushbuttons, switches, potmeter, encoder, servo
+#include <Arduino.h>
 
-// BEWARE: If servo.h in included, pins 9,10 do NOT work as analogWrite()
+// Ruud Boer, 2023, July
+// Demo box with 6 LEDs, 2 pushbuttons, 2switches, 1 potmeter, 1 encoder, 1 servo
 
-#define FADE_INTERVAL 2500 // [us]
-#define FADE_LOW         1 // PWM value 0-255
-#define FADE_HIGH      255 // PWM value 0-255
-#define SERVO_INTERVAL  30 // [ms] time between servo steps, larger is slower
-#define SERVO_MIN        5 // servo angle 0-180 degrees
-#define SERVO_MAX      175 // servo angle 0-180 degrees
+// BEWARE: If servo.h in included, pins 9,10 do NOT work as PWM analogWrite()
+
+// SW0 fades LED_SW0 on/off
+// SW1 fades LED_SW1 on/off
+// PB0 short/long switches LED_PB0 on/off and toggles servo to MIN/MAX / 90.
+// PB1 short/long switches LED_PB1 on/off
+// PB2 encoder switches LED_BLINK blinking on/off
+// encoder changes interval time for LED_BLINK
+// potmeter is dimmer for LED_POTM, value is printed on monitor
+
+#define PWM_LED_LOW      1 // minimum PWM value for LEDs 0-255
+#define PWM_LED_HIGH   255 // maximum PWM value for LEDs 0-255
+#define SERVO_MIN        5 // [degrees] 0-180
+#define SERVO_MAX      175 // [degrees] 0-180
+#define FADE_INTERVAL   10 // [ms]
+#define SERVO_INTERVAL  30 // [ms] time between servo steps
 #define ENC_DEBOUNCE    10 // [ms] encoder debounce
 #define ENC_SLOW_FAST  200 // [ms] encoder slow / fast rotation limit
-
-#define BLINK_ON        50 // [ms]
-#define BLINK_OFF      450 // [ms]
-#define DEBOUNCE      1000 // [ms]
-#define SHORT_PRESS    100 // [ms]
+#define ENC_INC_FAST    10 // [ms] encoder in-/decrement with fast rotation
+#define BLINK_ON        20 // [ms]
+#define BLINK_OFF      500 // [ms] initial, value is changed via the encoder
+#define DEBOUNCE        50 // [ms]
+#define SHORT_PRESS    200 // [ms]
 #define LONG_PRESS    2000 // [ms]
 
+#define SERVO_PIN       12 
 #define ENCA_PIN        14 // A0 Encoder A or Data
 #define ENCB_PIN        15 // A1 Encoder B or Clock
 #define POTM_PIN        17 // A3 Potentiometer Analog In
-#define SERVO_PIN       18 // A4 Servo
 
-#define LED_FADE         0 // number of the LED to use for fade
-#define LED_POTMETER     1 // number of the LED to use for the potmeter analogRead value
-#define LED_ENCODER      2 // number of the LED to use for the encoder value
+byte swpin[]     = {8,9};      // SW0, SW1 input pins
+byte pbpin[]     = {10,11,16}; // PB0, PB1, encoder input pins
+byte digledpin[] = {2,4,7,13}; // digital LED outputs pins, 13 is PCB LED
+byte pwmledpin[] = {3,5,6};    // pwm LED output pins NOTE: marked ~ but don't use pins 9,10 with servo.h
 
-byte sw_pin[]  = {7,8};      // pin numbers of the switches
-byte pb_pin[]  = {2,4,16};   // pin numbers of the pushbuttons, 16 is the encoder-sw
-byte pwm_pin[] = {3,5,6,11}; // pin numbers of the PWM outputs (don't use 9,10 with servo.h)
-byte dig_pin[] = {9,10,13};  // pin numbers of the digital outputs
+/*
+#define LED_SW0   pwmledpin[0] // fades on off with SW1
+#define LED_SW1   pwmledpin[1] // fades on off with SW2
+#define LED_POTM  pwmledpin[2] // brightness follows potmeter
+#define LED_PB0   digledpin[0] // TO BE DEFINED WHAT IT DOES
+#define LED_PB1   digledpin[1] // TO BB DEFINED WHAT IT DOES
+#define LED_BLINK digledpin[2] // blinks with [ms] interval which is changed with the encoder 
+*/
 
-//////////////////////////////////////////////////////////////////////////
+////////////////////////
 // End of configuration
-//////////////////////////////////////////////////////////////////////////
+////////////////////////
 
-const int numsw  = sizeof(sw_pin);
-const int numpb  = sizeof(pb_pin);
-const int numpwm = sizeof(pwm_pin);
-const int numdig = sizeof(dig_pin);
-byte sw[numsw], sw_old[numsw], sw_change[numsw]; // switches, sw_change: H>L=0 L>H=1 none=2
-byte pb[numpb], pb_old[numpb];                   // pushbuttons
-byte servo_value, servo_target;
-byte encA, encA_old, encB;
-byte blinking, pushed;
-byte pwm_value[numpwm], pwm_target[numpwm], dig_value[numdig]; 
-int analog_reading, enc_value, enc_value_old;
-unsigned int st = 1;  // states & transitions
-unsigned long timetofade, timetoblink, timetomoveservo, enc_rot_time;
-unsigned long sw_time[numsw], pb_time[numpb];
+#include <Servo.h>
+Servo servo; // A servo is declared with the name 'servo'
 
-#include <Servo.h>  // Servo functions are now included
-Servo servo;        // A Servo is declared with the name servo
+const int numsw     = sizeof(swpin);
+const int numpb     = sizeof(pbpin);
+const int numdigled = sizeof(digledpin);
+const int numpwmled = sizeof(pwmledpin);
+byte sw[numsw], swchange[numsw]; // swchange: 0=HL, 1=LH, 2=none
+byte pb[numpb], pbstate[numpb], pbstateold[numpb]; // pushbuttons
+byte servovalue = 90, servotarget = 90;
+byte encA, encAold, encB;
+byte blinking;
+byte digled[numdigled], pwmledtarget[numpwmled], pwmledvalue[numpwmled];
+int analogreading, encvalue = BLINK_OFF, encvalueold = BLINK_OFF;
+unsigned int st = 1; // states & transitions
+unsigned long timetofade, timetoblink, timetomoveservo, encrottime;
+unsigned long swtime[numsw], pbtime[numpb];
 
 void setup() {
   pinMode(ENCA_PIN, INPUT_PULLUP);
   pinMode(ENCB_PIN, INPUT_PULLUP);
-  for (byte i=0; i<numpb; i++)  pinMode(pb_pin[i],INPUT_PULLUP);
-  for (byte i=0; i<numsw; i++)  pinMode(sw_pin[i],INPUT_PULLUP);
-  for (byte i=0; i<numdig; i++) pinMode(dig_pin[i],OUTPUT);
+  for (byte i=0; i<numpb;  i++) pinMode( pbpin[i], INPUT_PULLUP);
+  for (byte i=0; i<numsw;  i++) pinMode( swpin[i], INPUT_PULLUP);
+  for (byte i=0; i<numdigled; i++) pinMode(digledpin[i], OUTPUT);
   // pwm_pin[] are not specified as OUTPUT, they are analogWrite() PWM pins
-  servo.attach(SERVO_PIN,1000,2000); //servo connected (pin, [us] pulse min, [us] pulse max)
+  servo.attach(SERVO_PIN, 1000, 2000); //servo connected (pin, [us] pulse min, [us] pulse max)
   Serial.begin(9600);
   Serial.println();
   Serial.println("Started");
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Function: detect pushbuttons state change and return pushed time 
-//////////////////////////////////////////////////////////////////////////
-byte read_pb(byte i) {
-  pb[i] = digitalRead(pb_pin[i]); // read the push button
-  if(pb[i] != pb_old[i]) {        // state has changed
-    pb_old[i] = pb[i];
-    if(pb[i]==0) pb_time[i] = millis(); // 1 > 0 transition, start timer
-    else {                              // 0 > 1 transition, return push time
-      int time_pushed = millis() - pb_time[i];
-      if     (time_pushed > (int)LONG_PRESS)  return 2;
-      else if(time_pushed > (int)SHORT_PRESS) return 1;
-      else                                    return 0;
+//////////////////////////////////////////////////////////////////////////////
+// Function: read and store the states and changes of the switches, debounced
+//////////////////////////////////////////////////////////////////////////////
+void read_sw() {
+  for (byte i=0; i<numsw; i++) {
+    byte reading = digitalRead(swpin[i]);
+    if(sw[i]==reading) {
+      swtime[i] = millis();
+      swchange[i] = 2; // 2=no change
+    }
+    else if(millis() > swtime[i] + (unsigned long)DEBOUNCE) {
+      sw[i]        = reading; // store the current state 0 or 1
+      swchange[i]  = reading; // is either 0 or 1 after a change, is 2 after no change
     }
   }
-  else return 0;
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Function: determine encoder slow or fast rotation
-//////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Function: detect pushbutton presses and store 0=short-, 1=long-, 2=no press
+///////////////////////////////////////////////////////////////////////////////
+void read_pb() {
+  for (byte i=0; i<numpb; i++) {  
+    pbstate[i] = digitalRead(pbpin[i]); // read the push button
+    if(pbstate[i] != pbstateold[i]) {   // state has changed
+      pbstateold[i] = pbstate[i];
+      if(pbstate[i]==0) pbtime[i] = millis(); // HL transition, start timer
+      else {                                  // LH transition, store push time
+        int timepushed = millis() - pbtime[i];
+        if     (timepushed > (int)LONG_PRESS)  pb[i] = 1;
+        else if(timepushed > (int)SHORT_PRESS) pb[i] = 0;
+        else                                   pb[i] = 2; // not pushed
+      }
+    }
+    else pb[i] = 2; // not pushed
+  }
+}
+
+//////////////////////////////////////////////////
+// Function: detect encoder slow or fast rotation
+//////////////////////////////////////////////////
 int read_encoder() {
   int increment;
   encA = digitalRead(ENCA_PIN);
-  if((encA_old == 0) && (encA == 1)) { // 0 > 1 transition
-    if     ((millis() - enc_rot_time) > ENC_SLOW_FAST) increment =  1;
-    else if((millis() - enc_rot_time) > ENC_DEBOUNCE)  increment = 10;
-    else                                               increment =  0;
-    enc_rot_time = millis();
-    encA_old = encA;
+  if((encAold == 0) && (encA == 1)) { // 0 > 1 transition
+    if     ((millis() - encrottime) > ENC_SLOW_FAST) increment =  1;
+    else if((millis() - encrottime) > ENC_DEBOUNCE)  increment = 10;
+    else                                             increment =  0;
+    encrottime = millis();
+    encAold = encA;
     if(digitalRead(ENCB_PIN) == 1) return  increment;
     else                           return -increment;
   }
-  encA_old = encA;
+  encAold = encA;
   return 0;
 }
 
 void loop() {
-
-//////////////////////////////////////////////////////////////////////////
-// Read and store the state and transition of the switches, debounced
-//////////////////////////////////////////////////////////////////////////
-  for (byte i=0; i<numsw; i++) {
-    byte reading = digitalRead(sw_pin[i]);
-    if(sw[i]==reading) {
-      sw_time[i] = millis();
-      sw_change[i] = 2; // 2 = no change
-    }
-    else if(millis() > sw_time[i] + (unsigned long)DEBOUNCE) {
-      sw[i]        = reading;
-      sw_change[i] = reading;
-    }
-  } // end for
-
-//////////////////////////////////////////////////////////////////////////
-// Read and store if pushbuttons are pressed, short or long
-//////////////////////////////////////////////////////////////////////////
-  for (byte i=0; i<numpb; i++) pb[i] = read_pb(i);
-
-//////////////////////////////////////////////////////////////////////////
-// Example of state transition diagram, st<100: state, st>100: transition
-//////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+// State transition diagram, st<100=state, st>100=transition
+/////////////////////////////////////////////////////////////
   switch(st) {
-    case 1: // read pushbuttons (pb) and switches (sw)
-      if(pb[0]==1) st = 101; // Short press: move servo left / right
-      if(pb[0]==2) st = 102; // Long press : move servo to 90
-      if(pb[1]==1) dig_value[1] = 1; // Switch a LED on
-      if(pb[1]==2) dig_value[1] = 0; // Switch a LED off
-      if(sw_change[0]==0) Serial.println("Fade off");
-      if(sw_change[0]==1) Serial.println("Fade on");
-      if(sw_change[1]==0) Serial.println("Blink off");
-      if(sw_change[1]==1) Serial.println("Blink on");
-      if(sw[0]==1) pwm_target[LED_FADE] = FADE_HIGH;
-      else         pwm_target[LED_FADE] = FADE_LOW;
-      if(sw[1]==1) blinking = 1;
-      else         blinking = 0;
+    case 1: // STATE 1:
+      read_pb(); // Execute the function to read states of pushbuttons
+      read_sw(); // Execute the function to read states of switches
+      if(pb[0]==0) st = 101; // PB0 short press
+      if(pb[0]==1) st = 102; // PB0 long press
+      if(pb[1]==0) st = 103; // PB1 short press
+      if(pb[1]==1) st = 104; // PB1 long press
+      if(pb[2]==0) st = 109; // PB2 encoder short press
+      if(swchange[0]==0) st = 105; // SW0 HL
+      if(swchange[0]==1) st = 106; // SW0 LH
+      if(swchange[1]==0) st = 107; // SW1 HL
+      if(swchange[1]==1) st = 108; // SW1 LH
     break;
-    case 101: // Toggle servo position left / right
-      if(servo_target==SERVO_MIN) servo_target = SERVO_MAX;
-      else                        servo_target = SERVO_MIN;
-      Serial.print("Servo target = ");
-      Serial.println(servo_target);
-      st = 1;
+    case 101: // TRANS 1: move servo to min/max angle, turn PB0 LED on
+      digled[0] = 1;
+      if(servotarget==SERVO_MIN) servotarget = SERVO_MAX;
+      else                       servotarget = SERVO_MIN;
+      Serial.print("101 PB0 short: Servo target = ");
+      Serial.println(servotarget);
+      st = 1; // return to STATE 1
     break;
-    case 102: // Move servo to 90
-      servo_target = 90;
-      Serial.print("Servo target = ");
-      Serial.println(servo_target);
-      st = 1;
+    case 102: // TRANS 2: move servo to 90, turn PB0 LED off
+      digled[0] = 0;
+      servotarget = 90;
+      Serial.print("102 PB0  long: Servo target = ");
+      Serial.println(servotarget);
+      st = 1; // return to STATE 1
     break;
-  } // end switch
+    case 103: // TRANS 3: PB1 short: LED on
+      digled[1] = 1;
+      Serial.println("103 PB1 short: LED2 ON");
+      st = 1; // return to STATE 1
+    break;
+    case 104: // TRANS 4: LED2 off
+      digled[1] = 0;
+      Serial.println("104 PB1 long: LED2 OFF");
+      st = 1; // return to STATE 1
+    break;
+    case 105: // TRANS 5: Fade SW0 LED to HIGH
+      pwmledtarget[0] = PWM_LED_HIGH;
+      Serial.println("105 SW0 HL: LED0 fade HIGH");
+      st = 1; // return to STATE 1
+    break;
+    case 106: // TRANS 6: Fade SW0 LED to LOW
+      pwmledtarget[0] = PWM_LED_LOW;
+      Serial.println("106 SW0 LH: LED0 fade LOW");
+      st = 1; // return to STATE 1
+    break;
+    case 107: // TRANS 7: Fade SW1 LED to HIGH
+      pwmledtarget[1] = PWM_LED_HIGH;
+      Serial.println("107 SW1 HL: LED1 fade HIGH");
+      st = 1; // return to STATE 1
+    break;
+    case 108: // TRANS 8: Fade SW1 LED to LOW
+      pwmledtarget[1] = PWM_LED_LOW;
+      Serial.println("108 SW1 LH: LED1 fade LOW");
+      st = 1; // return to STATE 1
+    break;
+    case 109: // TRANS 9: start/stop blinking 
+      blinking = !blinking;
+      if(blinking) Serial.println("109 LED5 blink ON");
+      else         Serial.println("109 LED5 blink OFF"); 
+      st = 1; // return to STATE 1
+    break;
+  }   // end switch
 
-//////////////////////////////////////////////////////////////////////////
-// Blink the LED on dig_pin[0] with different on and off times
-//////////////////////////////////////////////////////////////////////////
-  if (blinking==1) {
+/////////////////////////////////
+// Blink the LED on digledpin[2]
+/////////////////////////////////
+  if (blinking) {
     if (millis() > timetoblink) {
-      if(dig_value[0]==0) { // LED is off, we turn it on now   
+      if(!digled[2]) { // LED is off, we turn it on now   
         timetoblink = millis() + (unsigned long)BLINK_ON;
-        dig_value[0] = 1;
+        digled[2] = 1;
       }
-      else {                // led is on, we turn it off now
-        timetoblink = millis() + (unsigned long)BLINK_OFF;
-        dig_value[0] = 0;
+      else {           // led is on, we turn it off now
+        timetoblink = millis() + (unsigned long)encvalue;
+        digled[2] = 0;
       }
     }
   }
-  else dig_value[0] = 0;
-  dig_value[2] = dig_value[0]; //  Also blink the on board LED on pin 13
+  else digled[2] = 0;
+  digled[3] = digled[2]; // Also blink the on board LED on pin 13
 
-//////////////////////////////////////////////////////////////////////////
-// Update a LED value if it's time, this creates a fade
-//////////////////////////////////////////////////////////////////////////
-  if (micros() > timetofade) {
-      timetofade = micros() + (unsigned long)FADE_INTERVAL;
-      if(pwm_value[LED_FADE] < pwm_target[LED_FADE]) pwm_value[LED_FADE]++;
-      if(pwm_value[LED_FADE] > pwm_target[LED_FADE]) pwm_value[LED_FADE]--;
+///////////////////////
+// Fade pwmled 0 and 1
+///////////////////////
+  if (millis() > timetofade) {
+    timetofade = millis() + (unsigned long)FADE_INTERVAL;
+    for (byte i=0; i<numpwmled; i++) {  
+      if(pwmledvalue[i] < pwmledtarget[i]) pwmledvalue[i]++;
+      if(pwmledvalue[i] > pwmledtarget[i]) pwmledvalue[i]--;
+    }
   }
 
-//////////////////////////////////////////////////////////////////////////
-// Move the servo a step closer to the target angle if it's time
-//////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// Move the servo a step closer to the target angle when it's time
+///////////////////////////////////////////////////////////////////
   if (millis() > timetomoveservo) {
     timetomoveservo = millis() + (unsigned long)SERVO_INTERVAL;
-    if(servo_value < servo_target) servo_value++;
-    if(servo_value > servo_target) servo_value--;
-    servo.write(servo_value);
+    if(servovalue < servotarget) servovalue++;
+    if(servovalue > servotarget) servovalue--;
+    servo.write(servovalue);
   }
 
-//////////////////////////////////////////////////////////////////////////
-// Read the analog input and change LED_POTMETER brightness accordingly
-//////////////////////////////////////////////////////////////////////////
-  analog_reading = analogRead(POTM_PIN);
-  pwm_value[LED_POTMETER] = analog_reading / 4;
+//////////////////////////////////////////
+// Read the analog input and set pwmled 2
+//////////////////////////////////////////
+  analogreading = analogRead(POTM_PIN);
+  pwmledvalue[2] = analogreading / 4;
   
-//////////////////////////////////////////////////////////////////////////
-// Read the digital encoder and change LED_ENCODER brightness accordingly
-//////////////////////////////////////////////////////////////////////////  
-  enc_value = enc_value + read_encoder();
-  if(enc_value > 255) enc_value = 255;
-  if(enc_value <   0) enc_value =   0;
-  if(enc_value != enc_value_old) {
-    pwm_value[LED_ENCODER] = enc_value;
-    Serial.println(enc_value);
-    enc_value_old = enc_value;
+////////////////////////////////////////////////////////////
+// Read the digital encoder and update encvalue accordingly
+//////////////////////////////////////////////////////////// 
+  encvalue = encvalue + read_encoder();
+  if(encvalue > 1000) encvalue = 1000;
+  if(encvalue <   20) encvalue =   20;
+  if(encvalue != encvalueold) {
+    encvalueold = encvalue;
+    Serial.println(encvalue);
   }
 
-//////////////////////////////////////////////////////////////////////////
-// Write the PWM and the digital values to the outputs
-////////////////////////////////////////////////////////////////////////// 
-  for (byte i=0; i<numpwm; i++) analogWrite(pwm_pin[i],pwm_value[i]);
-  for (byte i=0; i<numdig; i++) digitalWrite(dig_pin[i],dig_value[i]);
+////////////////////////////////////////////////////
+// Write the PWM- and digital values to the led outputs
+////////////////////////////////////////////////////
+  for (byte i=0; i<numpwmled; i++) analogWrite(pwmledpin[i], pwmledvalue[i]);
+  for (byte i=0; i<numdigled; i++) digitalWrite(digledpin[i], digled[i]);
+
 } // end loop
