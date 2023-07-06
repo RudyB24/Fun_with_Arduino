5aabb3593f71ef5805fb81efc8916c84656d52c3
Rudy Boer <47658556+RudyB24@users.noreply.github.com>
2023-07-06 14:16:46 +0200

    Update Pushbutton_Short_Long.ino

--- Pushbutton_Short_Long/Pushbutton_Short_Long.ino
+++ Pushbutton_Short_Long/Pushbutton_Short_Long.ino
@@ -1,57 +1,53 @@
 // Ruud Boer, March 2022
-// Pushbuttons are read via a function that returns:
-// 2 if pressed longer than LONG_PRESS
-// 1 if pressed longer than SHORT_PRESS
-// 0 if pressed shorter than SHORT_PRESS, this also functions as a debounce
+// Pushbuttons that make in input LOW are read via a function that returns:
+// 0 if pressed longer than SHORT_PRESS
+// 1 if pressed longer than LONG_PRESS
+// 2 if pressed shorter than SHORT_PRESS, which is regareded no press
 
 #define SHORT_PRESS  100 // [ms]
 #define LONG_PRESS  2000 // [ms]
 
-byte pb_pin[] = {4,5}; // Pin numbers of the pushbuttons
+byte pbpin[] = {4,5}; // Pin numbers of the pushbuttons
 
 ////////////////////////
 // End of configuration
 ////////////////////////
 
-const int numpb = sizeof(pb_pin);
-byte pbstate[numpb], pressed;
-unsigned long pb_time[numpb];
+const int numpb = sizeof(pbpin);
+byte pbstate[numpb];
+unsigned long pbtime[numpb];
 
 void setup() {
-  for (byte i=0; i<numpb; i++)  pinMode(pb_pin[i],INPUT_PULLUP);
+  for (byte i=0; i<numpb; i++)  pinMode(pbpin[i], INPUT_PULLUP);
   Serial.begin(9600);
   Serial.println("Started");
 }
 
-////////////////////////////////////////////////////////////////////
-// Function: detect pushbuttons state change and return pushed time 
-////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+// Function: detect pushbuttons state change and store 0=short, 1=long, 2=no press 
+///////////////////////////////////////////////////////////////////////////////////
 byte read_pb(byte i) {
-  byte reading = digitalRead(pb_pin[i]);   // read the push button
-  if(reading != pbstate[i]) {              // state has changed
+  byte reading = digitalRead(pbpin[i]); // read the push button
+  if (reading != pbstate[i]) {           // state has changed
     pbstate[i] = reading;
-    if(!pbstate[i]) pb_time[i] = millis(); // HL transition, start timer
-    else {                                 // LH transition, return push time
-      int time_pushed = millis() - pb_time[i];
-      if     (time_pushed > (int)LONG_PRESS)  return 2;
-      else if(time_pushed > (int)SHORT_PRESS) return 1;
-      else                                    return 0;
+    if (!reading) pbtime[i] = millis();  // HL transition, start timer
+    else {                              // LH transition, store push time
+      int timepushed = millis() - pbtime[i];
+      if      (timepushed > (int)LONG_PRESS)  return 1; // value is only here for one loop cycle
+      else if (timepushed > (int)SHORT_PRESS) return 0; // value is only here for one loop cycle
+      else                                    return 2; // not pushed
     }
   }
-  else return 0;
-} // End function read_pb()
+  else return 2; // not pushed
+}
 
 void loop() {
-//////////////////////////////////////////////////////////////////////////
-// Read the pushbuttons and do something based on long or short press
-//////////////////////////////////////////////////////////////////////////
   for (byte i=0; i<numpb; i++) {
-    pressed = read_pb(i); //0=not pressed, 1=short, 2=long
-    if(pressed) { // Button has been pressed longer than SHORT_PRESS [ms]
+    if (read_pb(i) != 2) { // 0=short, 1=long, 2=no press
       Serial.print("PB");
       Serial.print(i);
-      if (pressed==1) Serial.println(" short");
-      else            Serial.println(" long");
+      if (!pb[i]) Serial.println(" short"); // 0=short press
+      else        Serial.println(" long");  // 1= long press
     }
   }
 }
